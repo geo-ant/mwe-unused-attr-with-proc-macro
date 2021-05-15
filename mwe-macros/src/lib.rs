f53134cba2628a22787b7779b7cd53b79621eedd
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-05-15 10:02:57 +0200

    remove obsolete comments

--- mwe-macros/src/lib.rs
+++ mwe-macros/src/lib.rs
@@ -2,7 +2,5 @@ use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn dummy(_args: TokenStream, input: TokenStream) -> TokenStream {
-    // let func = parse_macro_input!(input as ItemFn);
-    // TokenStream::from(quote!(#func))
     input
 }
